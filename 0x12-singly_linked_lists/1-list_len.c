4e4371915f38bcacd9d8ad35cb1f117bab6e4e7f
Grace815 <kinyuagrace355@gmail.com>
2023-08-24 14:00:56 -0700

    updated

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,18 +3,18 @@
 
 /**
  * list_len - brings back the size of a linked list's elements
- * @h: pointer to the list_t list
+ * @h: the list_t list's pointer
  *
  * Return: size of h's elements
  */
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+size_t n = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+while (h)
+{
+n++;
+h = h->next;
+}
+return (n);
 }
